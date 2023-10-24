7f9d7e54c48507f6f800c20f8ef3c231ac5e076e
Raffaele MECOCCI <raffaele.mecocci@alten.it>
2023-10-24 18:28:08 +0200

    feat 2

--- src/main/java/com/manning/junitbook/ch13/passengers/Passenger.java
+++ src/main/java/com/manning/junitbook/ch13/passengers/Passenger.java
@@ -32,12 +32,12 @@ public class Passenger {
     private String identifier;
     private String name;
     private String countryCode;
+    private Flight flight;
 
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
             throw new RuntimeException("Invalid country code");
         }
-
         this.identifier = identifier;
         this.name = name;
         this.countryCode = countryCode;
@@ -53,6 +53,29 @@ public class Passenger {
 
     public String getCountryCode() {
         return countryCode;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public void joinFlight(Flight flight) {
+        Flight previousFlight = this.flight;
+        if (null != previousFlight) {
+            if(!previousFlight.removePassenger(this)) {
+                throw new RuntimeException("Cannot remove passenger");
+            }
+        }
+        setFlight(flight);
+        if(null != flight) {
+            if(!flight.addPassenger(this)) {
+                throw new RuntimeException("Cannot add passenger");
+            }
+        }
     }
 
     @Override
--- src/test/java/com/manning/junitbook/ch13/flightspassengers/FlightWithPassengersTest.java
+++ src/test/java/com/manning/junitbook/ch13/flightspassengers/FlightWithPassengersTest.java
@@ -33,13 +33,23 @@ public class FlightWithPassengersTest {
     private Flight flight = new Flight("AA123", 1);
 
     @Test
+    public void testPassengerJoinsFlight() {
+        Passenger passenger = new Passenger("123-45-6789", "John Smith", "US");
+        passenger.joinFlight(flight);
+        assertEquals(flight, passenger.getFlight());
+        assertEquals(1, flight.getNumberOfPassengers());
+    }
+
+    @Test
     public void testAddRemovePassengers() throws IOException {
         Passenger passenger = new Passenger("124-56-7890", "Michael Johnson", "US");
         assertTrue(flight.addPassenger(passenger));
         assertEquals(1, flight.getNumberOfPassengers());
+        assertEquals(flight, passenger.getFlight());
 
         assertTrue(flight.removePassenger(passenger));
         assertEquals(0, flight.getNumberOfPassengers());
+        assertEquals(null, passenger.getFlight());
     }
 
     @Test
